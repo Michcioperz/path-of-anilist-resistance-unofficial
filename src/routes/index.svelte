646d473322eedcd2efc42403f30efe24cbc8d87c
Andreas Nedbal <git@pixelde.su>
2021-09-07 01:33:41 +0200

    re-layout index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,14 +7,16 @@
   }
 </script>
 
-<p>This app is designed to maximize your anime watching productivity by helping you find the show on your Anilist that you are not currently watching, but are closest to finishing watching.</p>
+<p class="text-lg mb-2">This app is designed to maximize your anime watching productivity by helping you find the show on your Anilist that you are not currently watching, but are closest to finishing watching.</p>
 
-<p>This allows you to pick something new and still optimize for getting things done.</p>
+<p class="text-lg mb-2">This allows you to pick something new and still optimize for getting things done.</p>
 
 <form on:submit|preventDefault={e => goGoManiac()}>
   <p>
     <label for="username">Pick your warrior (your Anilist username):</label>
-    <input bind:value={username} name="username" pattern=".+" type="text">
+    <input class="block w-full px-2 py-2 mb-2 rounded" bind:value={username} name="username" pattern=".+" type="text">
   </p>
-  <p><input type="submit" value="Isekai me!"></p>
+  <p>
+    <input type="submit" class="inline-block p-2 bg-blue-500 text-white rounded" value="Isekai me!">
+  </p>
 </form>
